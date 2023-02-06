9e63ab791f3adadda3f12f4b7689c250b4880138
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-02-07 00:41:08 +1300

    add

--- src/App.js
+++ src/App.js
@@ -13,9 +13,14 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
-
         <Route path="dash" element={<DashLayout />}>
           <Route index element={<Welcome />} />
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+           </Route>
+           <Route path="users">
+            <Route index element={<UsersList/>} />
+          </Route>
         </Route>
       </Route>
     </Routes>
